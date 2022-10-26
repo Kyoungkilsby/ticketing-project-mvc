22cbbe8826a173188d22a37ccad2e5a6e46a0e51
kyoung <kyoungkilsby@gmail.com>
2022-10-26 09:13:46 -0400

    ticketing project start

---
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package com.cydeo.dto;
+
+import com.cydeo.enums.Gender;
+
+public class UserDTO {
+    private String firstName;
+    private String lastname;
+    private String userName;
+    private String passWord;
+    private boolean enabled;
+    private String phone;
+    private RoleDTO role;
+    private Gender gender;
+}
