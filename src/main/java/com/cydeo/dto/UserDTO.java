a17772b430690a22702cdc303b215ba91c2fa77c
kyoung <kyoungkilsby@gmail.com>
2022-10-26 12:24:43 -0400

    ticketing add controller

--- src/main/java/com/cydeo/dto/UserDTO.java
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -1,7 +1,13 @@
 package com.cydeo.dto;
 
 import com.cydeo.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
     private String firstName;
     private String lastname;
